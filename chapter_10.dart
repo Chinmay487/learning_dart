f794d586558517e86cb7a24ad26f5f5b658b77e2
Chinmay487 <pchinmay04@gmail.com>
2022-06-24 18:50:25 +0530

    chapter 10 done

---
+++ chapter_10.dart
@@ -0,0 +1,25 @@
+// Boolean
+
+
+void main(){
+
+    /*
+        boolean data type is used to check whether a given statement is true or
+        false.The true and false are two values of boolean type,which are both
+        compile-time constants.In Dart the numeric value 1 or 0 cannot be used to
+        specify the true or false.The bool keyword is used to represent the
+        Boolean value.
+    */
+
+    // declaring boolean variable
+
+    bool var1 = true;
+    bool var2 = false;
+
+    int a = 10;
+    int b = 20;
+    bool result = a < b;
+    print("The result is $result");
+
+
+}
