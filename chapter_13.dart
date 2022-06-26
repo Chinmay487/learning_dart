8c355eae6706eedfec20c1a4815c4c6353188cdc
Chinmay487 <pchinmay04@gmail.com>
2022-06-26 22:38:26 +0530

    chapter 13 done

---
+++ chapter_13.dart
@@ -0,0 +1,82 @@
+// Object oriented concepts
+
+
+// ****** Class ******
+/*
+    Dart classes are defined as the the blueprint of the associated
+    objects.A class is a user-defined data type that describes the 
+    characteristics and behavious of it.To get all properties of the
+    class, we must create an object of that class
+*/
+
+/*
+    class ClassName{
+        // fields
+        // getters and setters
+        // constructors
+        // functions
+    }
+*/
+
+// ****** Object ******
+/*
+    An object is a real-life entity such as a table,human,etc.The object
+    has two characteristics- state and behaviour.
+    We access the class propteties using an object of that class.
+*/
+
+/*
+    var objectName = new ClassName(<constructor_arguments>);
+    ClassName objectName = new ClassName(<constructor_arguments>);
+*/
+
+// ****** Inheritance ******
+/*
+    Dart supports inheritance,which is used to create new classes from
+    existing class.The class that to be extended is call parent/superclass
+    and newly created class is called child/subclass.
+    Dart provides extends keyword to inherit propeties of parent class
+    in child class
+*/
+
+/*
+    class ChildClass extends ParentClassP{
+
+    }
+*/
+
+// ****** interfaces ******
+/*
+    The interface is defined as a blueprint of the class.We can
+    declare methods and variables inside the interfce just like the
+    class but in interface only abstract declaratio of methods is 
+    provided.We can only define the function signature but not its body.
+    Another class can implement the interface.It is basically used for
+    data hiding.
+*/
+
+// ****** Abstract Class ******
+/*
+    A class that contains one or more abstract method is called an
+    abstract class.We can declare the abstract class using abstract
+    keyword followed by class declaration.
+*/
+
+/*
+    abstract class ClassName{
+        // Body if abstract class.
+    }
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
